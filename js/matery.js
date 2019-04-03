88d9b2fc50011947f7b6d524f6739f4e2cefdb54
ChenChenYa_ <726301387@qq.com>
2019-04-03 18:18:31 +0800

    Site updated: 2019-04-03 18:18:15

--- js/matery.js
+++ js/matery.js
@@ -13,7 +13,7 @@ $(function () {
     articleCardHover();
 
     /*菜单切换*/
-    $('.button-collapse').sideNav();
+    $('.sidenav').sidenav();
 
     /* 修复文章卡片 div 的宽度. */
     let fixPostCardWidth = function (srcId, targetId) {
@@ -74,23 +74,49 @@ $(function () {
 
         $('#articleContent img').each(function () {
             let imgPath = $(this).attr('src');
-            $(this).wrap('<div class="img-item" data-src="' + imgPath + '"></div>');
+            $(this).wrap('<div class="img-item" data-src="' + imgPath + '" data-sub-html=".caption"></div>');
+            // 图片添加阴影
+            $(this).addClass("img-shadow img-margin");
+            // 图片添加字幕
+            let alt = $(this).attr('alt');
+            let title = $(this).attr('title');
+            let captionText = "";
+            // 如果alt为空，title来替
+            if (alt === undefined || alt === "") {
+                if (title !== undefined && title !== "") {
+                    captionText = title;
+                }
+            } else {
+                captionText = alt;
+            }
+            // 字幕不空，添加之
+            if (captionText !== "") {
+                let captionDiv = document.createElement('div');
+                captionDiv.className = 'caption';
+                let captionEle = document.createElement('b');
+                captionEle.className = 'center-caption';
+                captionEle.innerText = captionText;
+                captionDiv.appendChild(captionEle);
+                this.insertAdjacentElement('afterend', captionDiv)
+            }
         });
         $('#articleContent, #myGallery').lightGallery({
-            selector: '.img-item'
+            selector: '.img-item',
+            // 启用字幕
+            subHtmlSelectorRelative: true
         });
 
         // progress bar init
         const progressElement = window.document.querySelector('.progress-bar');
-        new ScrollProgress((x, y) => {
-            progressElement.style.width = y * 100 + '%';
-        });
+        if (progressElement) {
+            new ScrollProgress((x, y) => {
+                progressElement.style.width = y * 100 + '%';
+            });
+        }
     };
     articleInit();
 
-    $('#toggleSearch').click(function () {
-        $('#searchModal').openModal();
-    });
+    $('.modal').modal();
 
     /*回到顶部*/
     $('#backTop').click(function () {
